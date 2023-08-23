a20ebb5d6def27734aa6e6f693a63fa78276a646
krishna <krishna.rayanapati@gmail.com>
2023-08-23 23:48:12 +0530

    learn terraform

--- 05-data-sources/main.tf
+++ 05-data-sources/main.tf
@@ -1,7 +1,10 @@
-data "aws_ami" "example" {
+data "aws_ami" "ami" {
   executable_users = ["self"]
   most_recent      = true
   name_regex       = "Centos-*"
   owners           = ["973714476881"]
+}
 
+output "ami" {
+  value = data.aws_ami.ami
 }
