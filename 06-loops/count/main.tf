39ed58354cf8226e68d68ad62663c22cf6fe24dc
krishna <krishna.rayanapati@gmail.com>
2023-08-24 11:49:07 +0530

    learn terraform - loops

---
+++ 06-loops/count/main.tf
@@ -0,0 +1,16 @@
+variable "components" {
+  default = ["frontend" , "mongodb"]
+}
+
+resource "aws_instance" "instance" {
+
+  count = length(var.components)
+
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.small"
+  vpc_security_group_ids = [ "sg-06e8755bb8e15ec5f" ]
+
+  tags = {
+    Name = ""
+  }
+}
