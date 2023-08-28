ea8135b508ff8757b2467dafb8048d6314cf227f
krishna <krishna.rayanapati@gmail.com>
2023-08-28 18:25:38 +0530

    learn terraform module

--- 07-module/local-module/data.tf
+++ 07-module/local-module/data.tf
@@ -1,8 +1,15 @@
-data "aws_ami_ids" "ami" {
+data "aws_ami" "ami" {
+  most_recent = true
   name_regex       = "Centos-*"
   owners           = ["973714476881"]
 }
 
 output "ami" {
-  value = data.aws_ami_ids.ami
+  value = data.aws_ami.ami
+}
+
+resource "aws_instance" "instance" {
+  ami           = data.aws_ami.ami
+  instance_type = "t3.small"
+  vpc_security_group_ids = [ "sg-06e8755bb8e15ec5f" ]
 }
