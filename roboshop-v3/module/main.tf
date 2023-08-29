d069b26c86f292df39c9ddb6120d7cc2a075d68c
krishna <krishna.rayanapati@gmail.com>
2023-08-29 19:41:53 +0530

    roboshop terraform module

--- roboshop-v3/module/main.tf
+++ roboshop-v3/module/main.tf
@@ -6,10 +6,6 @@ resource "aws_instance" "instance" {
   tags = {
     Name = var.name
   }
-
-  provisioner "local-exec" {
-    command = "sleep 60"
-  }
 }
 
 
@@ -21,4 +17,19 @@ resource "aws_route53_record" "record" {
   records = [ aws_instance.instance.private_ip ]
 }
 
+resource "null_resource" "ansible" {
+
+  depends_on = [
+    aws_route53_record.record
+  ]
+
+  provisioner "local-exec" {
+    command = <<EOF
+cd /home/centos/roboshop-ansible
+git pull
+sleep 30
+ansible-playbook -i ${var.name}-dev.kdevops72.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
+EOF
+  }
+}
 
